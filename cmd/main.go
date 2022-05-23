56f51e2c4c741630bf2e45bef896ddc4a5a1b29d
veda-p <101187369+veda-p@users.noreply.github.com>
2022-05-23 13:09:30 +0200

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"{{ .appName }}/app/routes"
+    "{{ .appName }}/pkg/utils"
+	"os"
 
+	"github.com/gofiber/fiber/v2"
+)
+
+func health(c *fiber.Ctx)error{
+    return c.JSON("Health Check from {{ .appName }}")
+
+}
+
+
+func main(){
+    os.Setenv("SERVER_PORT","{{ .appPort }}")
+   app := fiber.New() 
+   app.Get("/{{.appName}}/health", health)
+   {{ .routes }}
+   // Start server (with or without graceful shutdown).
+	if os.Getenv("STAGE_STATUS") == "prod" {
+		utils.StartServerWithGracefulShutdown(app)
+	} else {
+		utils.StartServer(app)
+	}
 }
