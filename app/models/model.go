fb7feacdf1e33143ac97693e6a47b41b3cd255ca
veda-p <101187369+veda-p@users.noreply.github.com>
2022-05-23 13:06:03 +0200

    Templates: main added

---
+++ app/models/model.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"{{ .appName }}/app/routes"
+    "{{ .appName }}/pkg/utils"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func health(c *fiber.Ctx)error{
+    return c.JSON("Health Check from {{ .appName }}")
+
+}
+
+func main(){
+    os.Setenv("SERVER_PORT","{{ .appPort }}")
+   app := fiber.New() 
+   app.Get("/{{.appName}}/health", health)
+   {{ .routes }}
+   // Start server (with or without graceful shutdown).
+	if os.Getenv("STAGE_STATUS") == "prod" {
+		utils.StartServerWithGracefulShutdown(app)
+	} else {
+		utils.StartServer(app)
+	}
+}
