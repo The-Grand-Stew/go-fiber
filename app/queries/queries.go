bdcf883cf8b23c373c6f208d524a262c876a6d6e
veda-p <101187369+veda-p@users.noreply.github.com>
2022-05-23 13:07:29 +0200

    Create queries.go

---
+++ app/queries/queries.go
@@ -0,0 +1,30 @@
+package queries
+
+import "{{ .AppName }}/app/models"
+
+type {{ .DomainName }} models.{{ .DomainName }}
+
+func (q *{{ .DomainName }})Create()(any,error){
+    // YOUR CODE GOES HERE
+    return nil,nil
+}
+
+func (q *{{ .DomainName }})Get()(any,error){
+    // YOUR CODE GOES HERE
+    return nil,nil
+}
+
+func (q *{{ .DomainName }})GetAll()(any,error){
+    // YOUR CODE GOES HERE
+    return nil,nil
+}
+
+func (q *{{ .DomainName }})Update()(any,error){
+    // YOUR CODE GOES HERE
+    return nil,nil
+}
+
+func (q *{{ .DomainName }})Delete()(any,error){
+    // YOUR CODE GOES HERE
+    return nil,nil
+}
